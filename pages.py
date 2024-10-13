3341c4d2066db5e0461aaf3bd46da1ace52fb152
jesssssssssssss <jessica_sharma11@hotmail.com>
2024-10-14 11:31:24 +1300

    Changing tk instances to ctk format

--- pages.py
+++ pages.py
@@ -1,51 +1,56 @@
-import tkinter as tk
+''' import tkinter as tk
 from tkinter import ttk
 from tkinter import font
-import ttkbootstrap as ttk
+import ttkbootstrap as ttk '''
 import customtkinter as ctk
 import customtkinter
-from customtkinter import FontManager
+
 
 class HomePage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
 
-        label = ctk.CTkLabel(self, text="CloakCast", font=("Lalezar", 70), text_color='#a63a50')
+        homePageContent = ctk.CTkFrame(master = self, fg_color='White')
+        homePageContent.pack(padx=500, pady=250)
+
+
+        label = ctk.CTkLabel(homePageContent, text="CloakCast", font=("Lalezar", 70), text_color=("#a63a50", "#a63a50"), fg_color="white")
         label.pack(pady=20)
 
-        embed_button = ctk.CTkButton(self, text="Embed", command=lambda: controller.show_frame(EmbedPage))
-        embed_button.pack(pady=10) 
+        embedButton = ctk.CTkButton(homePageContent, text="Embed", command=lambda: controller.show_frame(EmbedPage))
+        embedButton.pack(pady=10) 
 
         #extract_button = ctk.CTkButton(self, text="Extract", command=lambda: controller.show_frame(ExtractPage))
         #extract_button.pack(pady=10)
+        # we can add these later
 
 class EmbedPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
-        embedPage = ctk.CTkFrame(master = self, fg_color='White') # this is the content window
-        embedPage.pack(padx=500, pady=250)
+        embedPageContent = ctk.CTkFrame(master = self, fg_color='White') # this is the content window
+        embedPageContent.pack(padx=500, pady=250)
 
-        label = ctk.CTkLabel(embedPage, text="Embed Page", font=customtkinter.CTkFont(family='Calibri', size=30))
+        label = ctk.CTkLabel(embedPageContent, text="Embed Page", font=customtkinter.CTkFont(family='Calibri', size=30))
         label.pack(pady=20)
 
         #Cover Media Frame
-        coverMediaFrame = ctk.CTkFrame(embedPage)
+        coverMediaFrame = ctk.CTkFrame(embedPageContent)
         
 
-        coverMedia_label = ctk.CTkLabel(embedPage, text='Cover Media', font=customtkinter.CTkFont(family='Calibri', size=30))
+        coverMedia_label = ctk.CTkLabel(embedPageContent, text='Cover Media', font=customtkinter.CTkFont(family='Calibri', size=30))
         coverMedia_label.pack()
 
         #Audio upload placeholder
-        audioUpload_label = tk.Label(coverMediaFrame, text='Upload Audio', borderwidth=8, relief='solid', font='Calibri 20')
+        audioUpload_label = ctk.CTkLabel(coverMediaFrame, text='Upload Audio', font=('Calibri', 20)) # , borderwidth=8, relief='solid' - figure our how to use in ctk
         audioUpload_label.pack(pady=10)
 
         #audioFile frame placeholder
-        audioFileFrame = ttk.Frame(master=coverMediaFrame) #frame
+        audioFileFrame = ctk.CTkFrame(master=coverMediaFrame) #frame
 
-        audioFile_label = tk.Label(master=audioFileFrame, width=30, borderwidth=8, relief='solid') #File display placeholder
-        deleteButton = ttk.Button(master=audioFileFrame, text='x', width=3) #Trash icon button placeholder
+        audioFile_label = ctk.CTkLabel(master=audioFileFrame, width=30) #File display placeholder -- these: borderwidth=8, relief='solid' - figure our how to use in ctk
+        deleteButton = ctk.CTkButton(master=audioFileFrame, text='x', width=3) #Trash icon button placeholder
 
         #Calling 
         audioFile_label.pack(side='left')
@@ -55,19 +60,19 @@ class EmbedPage(ctk.CTkFrame):
         coverMediaFrame.pack(side='left')
 
         #Hidden Data Frame
-        hiddenDataFrame = ttk.Frame(master=embedPage)
+        hiddenDataFrame = ctk.CTkFrame(master=embedPageContent)
 
-        hiddenData_label = ttk.Label(master=hiddenDataFrame, text='Hidden Data',font = 'Calibri 30')
+        hiddenData_label = ctk.CTkLabel(master=hiddenDataFrame, text='Hidden Data',font=('Calibri', 30))
         #Input field
-        input = ttk.Entry(master=hiddenDataFrame)
-        inputString =tk.StringVar()
-        input = tk.Entry(master=hiddenDataFrame, textvariable=inputString, borderwidth=8, relief='solid')
+        input = ctk.CTkEntry(master=hiddenDataFrame)
+        inputString = ctk.StringVar()
+        input = ctk.CTkEntry(master=hiddenDataFrame, textvariable=inputString) # , borderwidth=8, relief='solid'
 
         #or
-        orLabel = tk.Label(master=hiddenDataFrame, text='Or', font='Calibri 20')
+        orLabel = ctk.CTkLabel(master=hiddenDataFrame, text='Or', font=('Calibri', 20))
 
         #File upload placeholder
-        fileUpload_label = tk.Label(master=hiddenDataFrame, text='Upload File', borderwidth=8, relief='solid', font='Calibri 20')
+        fileUpload_label = ctk.CTkLabel(master=hiddenDataFrame, text='Upload File', font=('Calibri', 20)) # , borderwidth=8, relief='solid'
 
         #Calling
         hiddenData_label.pack()
@@ -76,10 +81,10 @@ class EmbedPage(ctk.CTkFrame):
         fileUpload_label.pack()
 
         #File frame placeholder
-        fileFrame = ttk.Frame(master=hiddenDataFrame) #frame
+        fileFrame = ctk.CTkFrame(master=hiddenDataFrame) #frame
 
-        file_label = tk.Label(master=fileFrame, width=30, borderwidth=8, relief='solid') #File display placeholder
-        deleteButton = ttk.Button(master=fileFrame, text='x', width=3) #Trash icon button placeholder
+        file_label = ctk.CTkLabel(master=fileFrame, width=30 ) #File display placeholder -- , borderwidth=8, relief='solid'
+        deleteButton = ctk.CTkButton(master=fileFrame, text='x', width=3) #Trash icon button placeholder
 
         #Calling 
         file_label.pack(side='left')
