9c06da968754c28043301d7b6abde1e1eeb12471
Dannielle <00danstar@gmail.com>
2024-10-18 09:29:43 +1300

    Adding the .grid embed page content

--- pages.py
+++ pages.py
@@ -6,6 +6,7 @@ import customtkinter as ctk
 import customtkinter
 from PIL import Image
 import os
+from tkinter import filedialog
 
 # I had to remove the tkinter imports, because they were overriding the CTK fomatting! So there are a few lines which still need converting
 # *** ********************
@@ -116,70 +117,128 @@ class EmbedPage(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
+        #Functions--------------------
+
+        #Variable holding selected audio file
+        selectedAudioFile = ctk.StringVar()
+        selectedAudioFile = set("No File Selected")
+ 
+        #Function to browse and open audio file
+        def openAudioFile():
+          filePath = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3")])
+          if filePath:
+           print(f"Selected file: {filePath}")
+ 
+        #Function to delete selected audio file
+        def deleteAudio():
+            selectedAudioFile = set("No File Selected")
+            deleteButton.configure(state=ctk.NORMAL) #Active only when a file has been selected
+
+        #Content--------------------
+
         embedPageContent = ctk.CTkFrame(master = self, fg_color='White') # this is the content window
-        #embedPageContent.pack(padx=500, pady=250)
+        embedPageContent.grid(padx=500, pady=250)
 
-        label = ctk.CTkLabel(embedPageContent, text="Embed Page", font=customtkinter.CTkFont(family='Calibri', size=30))
-       # label.pack(pady=20)
-
-        #Cover Media Frame
-        coverMediaFrame = ctk.CTkFrame(embedPageContent)
+        #Cover Media--------------------
+ 
+        coverMediaLabel = ctk.CTkLabel(master=embedPageContent, text='Cover Media', text_color='#a63a50', font=('lalezar', 40))
+        coverMediaLabel.grid(row=0, column=0)
         
-
-        coverMedia_label = ctk.CTkLabel(embedPageContent, text='Cover Media', font=customtkinter.CTkFont(family='Calibri', size=30))
-        #coverMedia_label.pack()
-
-        #Audio upload placeholder
-        audioUpload_label = ctk.CTkLabel(coverMediaFrame, text='Upload Audio', font=('Calibri', 20)) #  borderwidth=8, relief='solid' - figure our how to use in ctk
-       #audioUpload_label.pack(pady=10)
-
-        #audioFile frame placeholder
-        audioFileFrame = ctk.CTkFrame(master=coverMediaFrame) #frame
-
-        audioFile_label = ctk.CTkLabel(master=audioFileFrame, width=30) #File display placeholder --        # these: borderwidth=8, relief='solid' - figure our how to use in ctk
-        deleteButton = ctk.CTkButton(master=audioFileFrame, text='x', width=3) #Trash icon button placeholder
-
-        #Calling 
-       # audioFile_label.pack(side='left')
-        #deleteButton.pack(side='left')
-        #audioFileFrame.pack()
-        #Cover frame
-        #coverMediaFrame.pack(side='left')
-
-        #Hidden Data Frame
-        hiddenDataFrame = ctk.CTkFrame(master=embedPageContent)
-
-        hiddenData_label = ctk.CTkLabel(master=hiddenDataFrame, text='Hidden Data',font=('Calibri', 30))
+        #Audio upload button
+        audioUploadButton = ctk.CTkButton(master=embedPageContent,
+                                    text='Upload Audio',
+                                    text_color='#a63a50',
+                                    border_width= 3,
+                                    border_color='#393839',
+                                    corner_radius = 10,
+                                    fg_color= '#FFFFFF',
+                                    font=('Lalezar', 30),
+                                    command=openAudioFile) #Needs to be added
+        audioUploadButton.grid(row=1, column=0, pady=20)
+        
+        
+        #Chosen audio display
+        audioFileLabel = ctk.CTkLabel(master=embedPageContent,
+                                    textvariable= selectedAudioFile,
+                                    fg_color= 'blue',
+                                    corner_radius = 10)
+        audioFileLabel.grid(row=2, column=0)
+        
+        #Delete button for chosen audio file display
+        deleteButton = ctk.CTkButton(master=embedPageContent, text='x', width=3, command= deleteAudio) #Trash icon button placeholder
+        deleteButton.grid(row=2, column=2)
+        
+        
+        #Access Code--------------------
+        
+        #Header
+        accessCodeLabel = ctk.CTkLabel(master=embedPageContent, text='Access Code', text_color='#a63a50', font=('lalezar', 40))
+        accessCodeLabel.grid(row=3, column=0)
+        
+        enterCodeLabel = ctk.CTkLabel(master=embedPageContent, text='Enter Code', text_color='#393839', font=('lalezar', 20))
+        enterCodeLabel.grid(row=4, column=0)
+        
         #Input field
-        input = ctk.CTkEntry(master=hiddenDataFrame)
-        inputString = ctk.StringVar()
-        input = ctk.CTkEntry(master=hiddenDataFrame, textvariable=inputString) # , borderwidth=8, relief='solid'
-
+        inputAccessCode = ctk.CTkEntry(master=embedPageContent)
+        #took away string variable for placeholder text to work
+        inputAccessCode = ctk.CTkEntry(master=embedPageContent, placeholder_text='...', placeholder_text_color='#393839')
+        inputAccessCode.grid(row=5, column=0)
+        
+        #View access code button
+        viewAccessCode = ctk.CTkButton(master=embedPageContent, text='o', width=3) #eye icon button placeholder
+        viewAccessCode.grid(row=5, column=1)
+        
+        confirmCodeLabel = ctk.CTkLabel(master=embedPageContent, text='Confirm Code', width=50, text_color='#393839', font=('lalezar', 20))
+        confirmCodeLabel.grid(row=6, column=0)
+        
+        #Confirm input field
+        #Input field
+        ConfirmAccessCode = ctk.CTkEntry(master=embedPageContent)
+        #took away string variable for placeholder text to work
+        ConfirmAccessCode = ctk.CTkEntry(master=embedPageContent, placeholder_text='...', placeholder_text_color='#393839')
+        ConfirmAccessCode.grid(row=7, column=0)
+        
+        #View confirmed access code button
+        viewConfirmedAccessCode = ctk.CTkButton(master=embedPageContent, text='-', width=3) #eye icon button placeholder
+        viewConfirmedAccessCode.grid(row=7, column=1)
+        
+        
+        #Hidden Data--------------------
+        
+        hiddenDataLabel = ctk.CTkLabel(master=embedPageContent, text='Hidden Data', text_color='#a63a50', font=('Lalezar', 40))
+        hiddenDataLabel.grid(row=0, column=3)
+        
+        #Input field
+        input = ctk.CTkEntry(master=embedPageContent)
+        #took away string variable for placeholder text to work
+        input = ctk.CTkEntry(master=embedPageContent, placeholder_text='Enter Message...', placeholder_text_color='#393839')
+        input.grid(row=1, column=3)
+        
         #or
-        orLabel = ctk.CTkLabel(master=hiddenDataFrame, text='Or', font=('Calibri', 20))
-
-        #File upload placeholder
-        fileUpload_label = ctk.CTkLabel(master=hiddenDataFrame, text='Upload File', font=('Calibri', 20)) # , borderwidth=8, relief='solid'
-
-        #Calling
-       # hiddenData_label.pack()
-       # input.pack()
-       # orLabel.pack()
-       # fileUpload_label.pack()
-
-        #File frame placeholder
-        fileFrame = ctk.CTkFrame(master=hiddenDataFrame) #frame
-
-        file_label = ctk.CTkLabel(master=fileFrame, width=30 ) #File display placeholder -- , borderwidth=8, relief='solid'
-        deleteButton = ctk.CTkButton(master=fileFrame, text='x', width=3) #Trash icon button placeholder
-
-        #Calling 
-       # file_label.pack(side='left')
-      #  deleteButton.pack(side='left')
-      #  fileFrame.pack(pady=10)
-
-       # hiddenDataFrame.pack(side='left')
-
+        orLabel = ctk.CTkLabel(master=embedPageContent, text='Or', font=('Lalezar', 20))
+        orLabel.grid(row=2, column=3)
+        
+        #Text file upload button
+        audioUploadButton = ctk.CTkButton(master=embedPageContent,
+                                    text='Upload File',
+                                    text_color='#a63a50',
+                                    border_width= 3,
+                                    border_color='#393839',
+                                    corner_radius = 10,
+                                    fg_color= '#FFFFFF',
+                                    font=('Lalezar', 30)) #Needs to be added
+        audioUploadButton.grid(row=3, column=3, pady=20)
+        
+        #Chosen text file display placeholder
+        fileUploadLabel = ctk.CTkLabel(master=embedPageContent,
+                                    text='fiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiile',
+                                    fg_color= 'red',
+                                    corner_radius = 10) #File display placeholder
+        fileUploadLabel.grid(row=4, column=3)
+        
+        #Delete button for chosen Text file display
+        deleteButton = ctk.CTkButton(master=embedPageContent, text='x', width=3) #Trash icon button placeholder
+        deleteButton.grid(row=4, column=4)
         
         #back_button = ctk.CTkButton(embedPage, text="Back to Home", command=lambda: controller.show_frame(HomePage))
         #back_button.pack(pady=10)
